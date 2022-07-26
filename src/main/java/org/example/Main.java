a4f695cb91ad4f18b494ea6447ce99b054f864ff
Jay M <jaymansmann@yahoo.com>
2022-07-26 11:21:30 -0400

    fresh, no class code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -53,15 +53,6 @@ public class Main {
 //            stringToLength.put("angry", 5);
 //
 //        }, "stl4");
-
-        // Scenario: you are making a desktop app that lets the user see the weather forecast
-        // So the user is typing peacefully in the desktop
-        // Oh no! The desktop app decided it's time to pull updated weather information
-        // Do you want the desktop app to lock up for 5 seconds while it pulls the weather information from a server?
-
-        // Imagine you have a web app that takes in HTTP requests, (ex: Google server that receives search requests)
-
-
 //        stl1.start();
 //        stl2.start();
 //        stl3.start();
@@ -79,52 +70,43 @@ public class Main {
         // And let's add them to a map with two keys: true and false.
         // true's value will be a list of valid words that start with A.
         // false's value will be a list of Strings that aren't valid words that start with A
-        Map<Boolean, List<String>> results = new HashMap<>();
-        results.put(true, new ArrayList<>());
-        results.put(false, new ArrayList<>());
-//        SimpleMap<Boolean, List<String>> results = new JConcurrentMap<>();
-       String inputA = "a,apple,app,angry,argghhhhhhh";
-        String inputB = "and,angel,actual,altruistic";
-        Dictionary dictionary = Dictionary.getInstance();
-        Thread t3 = new Thread(() -> {
-            String[] inputArr = inputA.split(",");
-            for (String string : inputArr) {
-                System.out.println(Thread.currentThread() + " " + string + " being added to " + results);
-                boolean isWord = dictionary.isValidWord(string);
-
-                doWork(string, results, isWord);
-            }
-        }, "t3");
-
-        Thread t4 = new Thread(() -> {
-            String[] inputArr = inputB.split(",");
-            for (String string : inputArr) {
-                boolean isWord = dictionary.isValidWord(string);
-                System.out.println(Thread.currentThread() + " " + string + " being added to " + results);
-                doWork(string, results, isWord);
-                if(string.equals("altruistic")) {
-                    removeSomething(results);
-                }
-
-            }
-        }, "t4");
-        t3.start();
-        t4.start();
-        t3.join();
-        t4.join();
-        System.out.println(results);
-    }
-
-    private static synchronized void doWork(String string, Map<Boolean, List<String>> results, boolean isWord) {
-        if (results.containsKey(isWord)) {
-            // we don't know what will happen between lines 95 and 97
-            results.get(isWord).add(string);
-        } else {
-            results.put(isWord, new ArrayList<>(Arrays.asList(string)));
-        }
-    }
-
-    private static synchronized void removeSomething(Map<Boolean, List<String>> results) {
-        results.clear();
+//        Map<Boolean, List<String>> results = new HashMap<>();
+//       String inputA = "a,apple,app,angry,argghhhhhhh";
+//        String inputB = "and,angel,actual,altruistic";
+//        Dictionary dictionary = Dictionary.getInstance();
+//        Thread t3 = new Thread(() -> {
+//            String[] inputArr = inputA.split(",");
+//            for (String string : inputArr) {
+//                System.out.println(Thread.currentThread() + " " + string + " being added to " + results);
+//                boolean isWord = dictionary.isValidWord(string);
+//                if (results.containsKey(isWord)) {
+//                    // we don't know what will happen between the time the line above runs and the time the line
+//                    // below runs
+//                    results.get(isWord).add(string);
+//                } else {
+//                    results.put(isWord, new ArrayList<>(Arrays.asList(string)));
+//                }
+//            }
+//        }, "t3");
+//
+//        Thread t4 = new Thread(() -> {
+//            String[] inputArr = inputB.split(",");
+//            for (String string : inputArr) {
+//                boolean isWord = dictionary.isValidWord(string);
+//                System.out.println(Thread.currentThread() + " " + string + " being added to " + results);
+//                if (results.containsKey(isWord)) {
+//                    // we don't know what will happen between lines 95 and 97
+//                    results.get(isWord).add(string);
+//                } else {
+//                    results.put(isWord, new ArrayList<>(Arrays.asList(string)));
+//                }
+//
+//            }
+//        }, "t4");
+//        t3.start();
+//        t4.start();
+//        t3.join();
+//        t4.join();
+//        System.out.println(results);
     }
 }
--- src/main/java/org/example/map/JConcurrentMap.java
+++ src/main/java/org/example/map/JConcurrentMap.java
@@ -1,47 +1,24 @@
 package org.example.map;
 
-import java.util.AbstractMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class JConcurrentMap<K, V> implements SimpleMap<K, V>{
-    private Set<Map.Entry<K, V>> keyValuePairs = new HashSet<>();
     @Override
-    public synchronized void put(K key, V value) {
-       keyValuePairs
-               .stream()
-               .filter(keyValuePair -> keyValuePair.getKey().equals(key))
-               .findFirst()
-               .ifPresentOrElse(entry -> {
-                   entry.setValue(value);
-               }, () -> keyValuePairs.add(new AbstractMap.SimpleEntry<>(key, value)));
+    public  void put(K key, V value) {
     }
 
     @Override
     public synchronized V get(K key) {
-        Map.Entry<K, V> pair = keyValuePairs
-                .stream()
-                .filter(keyValuePair -> keyValuePair.getKey().equals(key))
-                .findFirst()
-                .orElse(null);
-        if(pair == null)
-        {
-            return null;
-        }
-        return pair.getValue();
+        return null;
 
     }
 
     @Override
     public synchronized boolean containsKey(K key) {
-        return keyValuePairs
-                .stream().anyMatch(keyValuePair -> keyValuePair.getKey().equals(key));
+        return false;
     }
 
     @Override
     public String toString() {
-        return keyValuePairs.toString();
+        return "";
     }
 }
 
